e7da2b236e431ace682d2f4e89eedd4bbc7c8ea8
tony tolver <andrademaresantonio00@gmail.com>
2023-04-06 23:33:38 -0600

    Create - Get all clientes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");  // Parsear el body de las peticiones - fundamental para poder leer los datos que vienen en el body de las peticiones
 
 // conectar mongo 
 mongoose.Promise = global.Promise;
@@ -10,6 +11,10 @@ mongoose.connect("mongodb://localhost/restapis",{
 
 // Create server
 const app = express();
+
+// Habilitar body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //rutas de la app
 app.use("/", routes());
